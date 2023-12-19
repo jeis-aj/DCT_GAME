def4c0e4c035e3440cacdf97d7d519269f05104e
jeis-aj <amjyo539@gmail.com>
2023-12-19 14:14:58 +0530

    reload animation add

--- GunBoard/GunBoard.ino
+++ GunBoard/GunBoard.ino
@@ -15,7 +15,7 @@ IRsend irsend;
 const int max_attempt_count = 8;		 // Max attempt for a person
 const int trigger_pin = 2;			// Trigger button pin
 int Reloading_ledPin = 13;			// Reloading LED Indicator 
-int led_arr[max_attempt_count] = {4,5,6,7,8,9,10,11};			// led pins
+int led_arr[max_attempt_count] = {4,5,6,7,8,9,10,11,12};			// led pins
 
 void setup()
 {
@@ -39,13 +39,31 @@ void loop() {
 		++count;
 	}
 	else{
+		/*
 		for(int led_pin: led_arr){
 			digitalWrite( led_pin, HIGH);
+			delay(1000);		// 10 seconds Reloading delay
 		}
+		*/
+
 		digitalWrite( Reloading_ledPin, HIGH );
 		Serial.println("Reloading....");
 		count = 0 ;
-		delay(10000);		// 10 seconds Reloading delay
+		reload_animation();
 	}
 
+}
+#define max_animation_count 5
+void reload_animation(void) {
+	for ( int count = 0; count < max_animation_count ; ++count ){
+		for(int led_pin: led_arr){
+			digitalWrite( led_pin, HIGH);
+			delay(100);
+		}
+			delay(400);
+		for(int led_pin: led_arr){
+			digitalWrite( led_pin, LOW);
+			delay(100);
+		}
+	}
 }
