50041fa2e8a12c6c8fd922fbb17109e8c3c40a32
arunjyothishk <arunjyothish.mec@gmail.com>
2023-12-20 07:15:07 +0530

    decorate display

--- app/display.cpp
+++ app/display.cpp
@@ -13,42 +13,48 @@ int main(){
 	/* while ( system("mpg123 bgm.mp3 >> /dev/null 2>&1") ){ } */
 }
 void display(int number){
+	system("clear");
+	system("echo '\n'");
+	system("echo '\n'");
+	system("echo '\n'");
+	system("echo '\n'");
+	system("echo '\n'");
 	switch (number){
 		case 1:
-			system("cat numbers/one");
+			system("cat numbers/one	| lolcat ");
 			break;
 		case 2:
-			system("cat numbers/two");
+			system("cat numbers/two	| lolcat ");
 			break;
 		case 3:
-			system("cat numbers/three");
+			system("cat numbers/three	| lolcat ");
 			break;
 		case 4:
-			system("cat numbers/four");
+			system("cat numbers/four	| lolcat ");
 			break;
 		case 5:
-			system("cat numbers/five");
+			system("cat numbers/five	| lolcat ");
 			break;
 		case 6:
-			system("cat numbers/six");
+			system("cat numbers/six	| lolcat ");
 			break;
 		case 7:
-			system("cat numbers/seven");
+			system("cat numbers/seven	| lolcat ");
 			break;
 		case 8:
-			system("cat numbers/eight");
+			system("cat numbers/eight	| lolcat ");
 			break;
 		case 9:
-			system("cat numbers/nine");
+			system("cat numbers/nine	| lolcat ");
 			break;
 		case 10:
-			system("cat numbers/hurray");
+			system("cat numbers/hurray	| lolcat ");
 			break;
 		case 0:
-			system("cat numbers/zero");
+			system("cat numbers/zero	| lolcat ");
 			break;
 		case -1:
-			system("cat numbers/lost");
+			system("cat numbers/lost	| lolcat ");
 			break;
 	}
 }
